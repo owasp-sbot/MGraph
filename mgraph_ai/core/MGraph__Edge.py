f0e70cf265c3cd84ca8d7ef3c93bed58c56de6d0
Dinis Cruz <dinis.cruz@owasp.org>
2024-12-30 01:07:47 +0000

    refactored in original MGraph code from OSBot utils

---
+++ mgraph_ai/core/MGraph__Edge.py
@@ -0,0 +1,17 @@
+from typing import Dict, Any
+from osbot_utils.base_classes.Type_Safe import Type_Safe
+
+from mgraph_ai.core.MGraph__Node import MGraph__Node
+
+
+class MGraph__Edge(Type_Safe):
+    attributes : Dict[str, Any]
+    from_node  : MGraph__Node
+    label      : str
+    to_node    : MGraph__Node
+
+    def __str__(self):
+        return f'[Graph Edge] from "{self.from_node.key}" to "{self.to_node.key}" '
+
+    def data(self):
+        return self.__locals__()             # todo: see if there is a better way to do this (specialy as the edge objects gets more features and attributes)
